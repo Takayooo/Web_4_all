d55620a227b79be4ec5d7a8cf69434b55f52070b
Alexis^^ <deepix.alexis@gmail.com>
2026-03-30 15:11:40 +0200

    initialisatien de la base de données

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+$host = '127.0.0.1';
+$dbname = 'web4all';
+$user = 'root';
+$password = 'Root123!';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur BDD : " . $e->getMessage());
+}
